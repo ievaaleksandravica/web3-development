e3ce1cadf4083e05a42c227d12ef581fb3463151
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-02-18 12:22:04 +0100

    Define network for truffle

--- GreetingsTruffle/truffle.js
+++ GreetingsTruffle/truffle.js
@@ -2,6 +2,10 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*"
+    }
   }
 };
