0f2529753dbb76f21c7c028d71a6d71a00ba6807
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-02-20 11:43:01 +0100

    Define function for sell aricle

--- ChainList/contracts/ChainList.sol
+++ ChainList/contracts/ChainList.sol
@@ -1,3 +1,21 @@
 pragma solidity ^0.4.18;
 
-contract Chainlist
+contract Chainlist {
+    // state variables
+    address seller;
+    string name;
+    string description;
+    uint256 price;
+
+    // function to sell an article
+    function sellArticle(
+        string _name,
+        string _description,
+        uint256 _price
+    ) public {
+        seller = msg.sender;
+        name = _name;
+        description = _description;
+        price = _price;
+    }
+}
