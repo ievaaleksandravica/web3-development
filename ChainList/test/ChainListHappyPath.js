4550a04d4dea1587792aa0be166054ca2a6badaf
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-02-21 18:50:43 +0100

    writing tests

--- ChainList/test/ChainListHappyPath.js
+++ ChainList/test/ChainListHappyPath.js
@@ -3,5 +3,14 @@ var ChainList = artifacts.require("./ChainList.sol");
 
 //test suite
 contract("ChainList", function(accounts) {
-
+    it("should be initialized with empty values", function(){
+        return ChainList.deployed().then(function(instance) {
+            return instance.getArticle();
+        }).then(function(data) {
+            assert.equal(data[0], 0x0, "seller must be empty");
+            assert.equal(data[1], "", "article name must be empty");
+            assert.equal(data[2], "", "article description must be empty");
+            assert.equal(data[3].toNumber(), 0, "article price must be zero");
+        })
+    });
 })
