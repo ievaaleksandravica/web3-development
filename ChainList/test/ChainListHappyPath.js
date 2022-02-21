6fb732c8c2220879b61f24227c07bcaeb3472dec
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-02-21 18:24:55 +0100

    define test suite

---
+++ ChainList/test/ChainListHappyPath.js
@@ -0,0 +1,7 @@
+// loading the ChainList contract and wrapping in Truffle abstraction
+var ChainList = artifacts.require("./ChainList.sol");
+
+//test suite
+contract("ChainList", function(accounts) {
+
+})
