99c7e2c8945c9588f2ac2a8fc5efdca85250ca76
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-02-26 11:14:08 +0100

    Improve the test suite to check the event log

--- ChainList/test/ChainListHappyPath.js
+++ ChainList/test/ChainListHappyPath.js
@@ -33,5 +33,19 @@ contract("ChainList", function(accounts) {
             assert.equal(data[2], articleDescription, "article description must be " + articleDescription);
             assert.equal(data[3].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
         })
+    });
+
+    it("should trigger an event when a new article is sold", function() {
+        return ChainList.deployed().then(function(instance) {
+            chainListInstance = instance;
+            return chainListInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {from: seller}).then(function(receipt) {
+                assert.equal(receipt.logs.length, 1, "one event should have been triggered");
+                assert.equal(receipt.logs[0].event, "LogSellArticle", "event should be LogSellArticle");
+                assert.equal(receipt.logs[0].args._seller, seller, "event seller must be " + seller);
+                assert.equal(receipt.logs[0].args._name, articleName, "event seller must be " + articleName);
+                assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(articlePrice, "ether"), "event seller must be " + web3.toWei(articlePrice, "ether"));
+                assert.equal(receipt.logs[0].args._seller, seller, "event seller must be " + seller);
+            })
+        })
     })
 })
