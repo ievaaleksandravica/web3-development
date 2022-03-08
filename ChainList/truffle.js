6cf5d7a81ec38671086e757f4e2885bc2a427913
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-03-08 20:02:15 +0100

    cleanup

--- ChainList/truffle.js
+++ ChainList/truffle.js
@@ -11,8 +11,7 @@ module.exports = {
                host: "localhost",
                port: 8545,
                network_id: "4224",
-               gas: 47000000,
-               from: '0xa6af4272bd0150fbfbd70f569ada4a0a353ebf68'
+               gas: 47000000
           }
      }
 };
