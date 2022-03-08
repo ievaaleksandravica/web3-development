c73c89329d34328d81db88210af9c6111c892c25
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-03-08 19:18:01 +0100

    deployment to private geth node

--- ChainList/truffle.js
+++ ChainList/truffle.js
@@ -10,7 +10,8 @@ module.exports = {
           chainskills: {
                host: "localhost",
                port: 8545,
-               network_id: "4224"
+               network_id: "4224",
+               gas: 4700000
           }
      }
 };
