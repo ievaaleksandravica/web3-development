375863122329c02603d5f4af70bf910287e0d8f2
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-03-10 13:18:16 +0100

    run test suite

--- ChainList/test/ChainListExceptions.js
+++ ChainList/test/ChainListExceptions.js
@@ -5,6 +5,7 @@ var ChainList = artifacts.require("./ChainList.sol")
 contract("Chainlist", function(accounts) {
     var chainListInstance;
     var seller = accounts[1];
+    var buyer = accounts[2];
     var articleName = "article 1";
     var articleDescription = "Description for Article 1";
     var articlePrice = 10;
@@ -20,6 +21,16 @@ contract("Chainlist", function(accounts) {
             .then(assert.fail)
             .catch(function(error) {
                 assert(true)
+            })
+            .then(function() {
+                return chainListInstance.getArticle();
+            })
+            .then(function(data) {
+                assert.equal(data[0], 0x0, "seller must be empty");
+                assert.equal(data[1], 0x0, "buyer must be empty");
+                assert.equal(data[2], "", "article name must be empty");
+                assert.equal(data[3], "", "article description must be empty");
+                assert.equal(data[4].toNumber(), 0, "article price must be zero");
             })
         })
     })
