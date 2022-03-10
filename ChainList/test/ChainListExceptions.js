3fa738ce8fea3af9c798388e5c00e2fd4a8b5b24
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-03-10 13:09:26 +0100

    should throw an exception if you try to buy an article when there is no article for sale yet

---
+++ ChainList/test/ChainListExceptions.js
@@ -0,0 +1,18 @@
+// load the contract to be tested
+var ChainList = artifacts.require("./ChainList.sol")
+
+// test suite
+contract("Chainlist", function(accounts) {
+    var chainListInstance;
+    var seller = accounts[1];
+    var articleName = "article 1";
+    var articleDescription = "Description for Article 1";
+    var articlePrice = 10;
+
+    // no article for sale yet
+    it("should throw an exception if you try to buy an article when there is no article for sale yet", function() {
+        
+    })
+
+
+});
