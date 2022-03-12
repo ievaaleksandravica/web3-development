d0882671c33489730647e2e289f28f728edff677
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-03-12 09:37:58 +0100

    Chain the chainlist contract to group all the article fields

--- ChainList/contracts/ChainList.sol
+++ ChainList/contracts/ChainList.sol
@@ -1,12 +1,18 @@
 pragma solidity ^0.4.18;
 
 contract ChainList {
+    // custom types
+    struct Article {
+        uint: id;
+        address seller;
+        address buyer;
+        string name;
+        string description;
+        uint256 price;
+    }
+
     // state variables
-    address seller;
-    address buyer;
-    string name;
-    string description;
-    uint256 price;
+   
 
     // events
     event LogSellArticle(address indexed _seller, string _name, uint256 _price);
