c12f7a6f955d6174309ab62fe4feb00641bc0415
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-03-20 10:50:52 +0100

    define constructor

--- ChainList/contracts/Ownable.sol
+++ ChainList/contracts/Ownable.sol
@@ -3,4 +3,15 @@ pragma solidity ^0.4.18;
 contract Ownable {
     // state variables
     address owner;
+
+    // modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // constructor
+    function Ownable() public {
+        owner = msg.sender;
+    }
 }
