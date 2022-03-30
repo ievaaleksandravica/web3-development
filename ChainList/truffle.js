9fc782de9a0923262540396f4518e9426374838c
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-03-30 16:36:29 +0200

    Update Truffle’s configuration

--- ChainList/truffle.js
+++ ChainList/truffle.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -12,6 +16,14 @@ module.exports = {
                port: 8545,
                network_id: "4224",
                gas: 4700000
+          },
+          ropsten: {
+               provider: function () {
+                    return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY);
+                },
+               network_id: 3,
+               gas: 4500000,
+               gasPrice: 10000000000
           }
      }
 };
