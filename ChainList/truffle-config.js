0d5cbe9c5f7f33a8e0ed4709c595f41baf4ab009
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-04-01 18:33:09 +0200

    change the truffle.js file name

--- ChainList/truffle-config.js
+++ ChainList/truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -5,7 +9,22 @@ module.exports = {
           ganache: {
                host: "localhost",
                port: 7545,
-               network_id: "*" // Match any network id
+               network_id: "*",
+               gas: 4700000 // Match any network id
+          },
+          chainskills: {
+               host: "localhost",
+               port: 8545,
+               network_id: "4224",
+               gas: 4700000
+          },
+          ropsten: {
+               provider: function () {
+                    return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY);
+                },
+               network_id: 3,
+               gas: 4500000,
+               gasPrice: 10000000000
           }
      }
 };
